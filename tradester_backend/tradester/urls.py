95c854a98bc9a1499662ca86032d3a52ba873c71
patriickdoyle <patrick.doyle.coding@gmail.com>
2023-02-26 18:44:16 -0500

    added skeleton code for views sign_in, register, and get_stock_data. Documented these views and gave each a url to be accessed at.

--- tradester_backend/tradester/urls.py
+++ tradester_backend/tradester/urls.py
@@ -4,4 +4,7 @@ from . import views
 
 urlpatterns = [
     path('', views.index, name='index'),
+    path('register/<str:_username>/<str:_password>/', views.register, name='register'),
+    path('sign_in/<str:_username>/<str:_password>/', views.sign_in, name='sign_in'),
+    path('get_stock_data/<str:_stock_symbol>/', views.get_stock_data, name='get_stock_data'),
 ]
--- tradester_backend/tradester/views.py
+++ tradester_backend/tradester/views.py
@@ -1,5 +1,8 @@
-from django.http import HttpResponse
+from django.http import HttpResponse, JsonResponse
 from django.shortcuts import render
+
+from tradester.models import Stock
+
 
 # Create your views here.
 def index(request):
@@ -7,3 +10,55 @@ def index(request):
     :template:`path/to/template.html`
     """
     return HttpResponse("Welcome to the Tradester index.")
+
+
+def register(request, _username, _password):
+    """
+    View to register a new user account
+
+    param request: the request object \n
+    type request: HttpRequest \n
+    param _username: inputted username \n
+    type _username: str \n
+    param _password: inputted password \n
+    type _password: str \n
+    return: HttpResponse object with 'Account creation successful' or 'Username is already taken' \n
+    rtype: HttpResponse
+    """
+
+    # TODO: Implement the account creation logic
+
+    return HttpResponse("register")
+
+
+def sign_in(request, _username: str, _password: str) -> HttpResponse:
+    """
+    View to sign in a user
+
+    param request: the request object \n
+    param _username: inputted username \n
+    param _password: inputted password \n
+    return: HttpResponse object with 'Sign in successful' or 'Invalid login credentials' \n
+    rtype: HttpResponse
+    """
+    # TODO: implement sign_in functionality
+    return HttpResponse("sign_in")
+
+
+def get_stock_data(request, _stock_symbol):
+    """
+    View to get data on a specified stock
+
+    param request: the request object \n
+    type request: HttpRequest \n
+    param _symbol: symbol of the stock to retrieve data for \n
+    type _symbol: str \n
+    return: HttpResponse object with JSON data of stock information or error message \n
+    rtype: HttpResponse
+    """
+
+    # TODO: Implement function logic to retrieve data on specified stock
+
+    # TODO: If stock data retrieved successfully, return as JSON in HttpResponse object
+    # TODO: If error occurs, return error message as JSON in HttpResponse object
+    return HttpResponse("get_stock_data")
