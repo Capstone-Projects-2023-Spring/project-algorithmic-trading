4e13e062ad34e26806e72505d0ed46ebad3dff25
patriickdoyle <patrick.doyle.coding@gmail.com>
2023-02-26 17:18:43 -0500

    added all models to the Django backend

--- tradester_backend/tradester/admin.py
+++ tradester_backend/tradester/admin.py
@@ -1,3 +1,7 @@
 from django.contrib import admin
-
+from .models import Portfolio, Stock, Portfolio_stock, Order
 # Register your models here.
+admin.site.register(Portfolio)
+admin.site.register(Stock)
+admin.site.register(Portfolio_stock)
+admin.site.register(Order)
--- tradester_backend/tradester_backend/settings.py
+++ tradester_backend/tradester_backend/settings.py
@@ -12,6 +12,8 @@ https://docs.djangoproject.com/en/4.1/ref/settings/
 
 from pathlib import Path
 
+import tradester.apps
+
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
 
@@ -31,6 +33,7 @@ ALLOWED_HOSTS = []
 # Application definition
 
 INSTALLED_APPS = [
+    'tradester.apps.TradesterConfig',
     'django.contrib.admindocs',
     'django.contrib.admin',
     'django.contrib.auth',
