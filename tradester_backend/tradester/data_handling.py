bc03e82c4fcb577dc5d18ef1d3458b61486fb579
patriickdoyle <patrick.doyle.coding@gmail.com>
2023-03-23 15:15:09 -0400

    created function in data_handling.py to call polygon api and return entries on the entire stock market. Also updated the Stock table in models.py with attributes returned from polygon.

--- tradester_backend/tradester/data_handling.py
+++ tradester_backend/tradester/data_handling.py
@@ -5,7 +5,6 @@ This Python module holds functions to perform operations on data
 such as loading data to the database and processing the data. These
 functions are called by views when the backend receives HTTP requests.
 
-Functions: fetch_stock_data()
 """
 import csv
 from io import StringIO
@@ -15,38 +14,17 @@ import requests
 from tradester.models import Stock
 #import pandas as pd
 
-def fetch_stock_data():
-    #get list of s&p 500 stocks
-    url = 'https://datahub.io/core/s-and-p-500-companies-financials/r/constituents.csv'
-    response = requests.get(url)
-    decoded_content = response.content.decode('utf-8')
-    reader = csv.reader(StringIO(decoded_content))
-    next(reader)  # skip header row
-    for row in reader:
-        stock_symbol = row[0]
-        if not Stock.objects.filter(stock_symbol=stock_symbol).exists():
-            # get data from Alpha Vantage
-            api_key = '2JMCN347HZ3BU9RC'
-            url = f'https://www.alphavantage.co/query?function=OVERVIEW&symbol={stock_symbol}&apikey={api_key}'
-            response = requests.get(url)
-            if response.status_code == 200:
-                data = response.json()
-                # create stock instance with data from API
-                stock = Stock(
-                    stock_symbol=stock_symbol,
-                    company_name=data.get('Name', ''),
-                    sector=data.get('Sector', ''),
-                    current_price=data.get('Price', ''),
-                    market_cap=data.get('MarketCapitalization', ''),
-                    dividend_yield=data.get('DividendYield', ''),
-                    earnings_per_share=data.get('EPS', ''),
-                    price_to_earnings_ratio=data.get('PERatio', ''),
-                    beta=data.get('Beta', ''),
-                    high_52=data.get('52WeekHigh', ''),
-                    low_52=data.get('52WeekLow', ''),
-                    avg_daily_volume=data.get('AverageDailyVolume', '')
-                )
-                stock.save()
+def fetch_daily_OHLC():
+    """
+    This calls the Polygon API to return data on the entire stock/equities market and load it to the database. 
+    Updates automatically each day.   
+    """
 
-    # make call to AlphaVantage for stock data
-    #ts = TimeSeries(key="2JMCN347HZ3BU9RC")#, output_format='pandas')
+    # API endpoint URL for batch stock quotes
+    url = f'https://api.polygon.io/v2/aggs/grouped/locale/us/market/stocks/2023-01-09?adjusted=true&apiKey=6B07KCTp2RUNh2zOybygjNYppflkxS_1'
+
+    # Send request to API and retrieve data
+    response = requests.get(url)
+    data = response.json()
+    print(data)
+    return data
--- tradester_backend/tradester/models.py
+++ tradester_backend/tradester/models.py
@@ -32,6 +32,12 @@ class Stock(models.Model):
     high_52 = models.DecimalField(max_digits=10, decimal_places=2, blank=True, null=True)
     low_52 = models.DecimalField(max_digits=10, decimal_places=2, blank=True, null=True)
     avg_daily_volume = models.IntegerField(blank=True, null=True)
+    daily_high = models.DecimalField(max_digits=10, decimal_places=2, blank=True, null=True)
+    daily_low = models.DecimalField(max_digits=10, decimal_places=2, blank=True, null=True)
+    daily_num_transactions = models.DecimalField(max_digits=10, decimal_places=2, blank=True, null=True)
+    daily_open_price = models.DecimalField(max_digits=10, decimal_places=2, blank=True, null=True)
+    daily_volume = models.DecimalField(max_digits=10, decimal_places=2, blank=True, null=True)
+    daily_vwap = models.DecimalField(max_digits=10, decimal_places=2, blank=True, null=True)
 
     def __str__(self):
         return self.stock_symbol
