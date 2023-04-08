2c072c7ef3ebf77c2b7e57f9bfbc609e34801e38
Jaffar Alzeidi <jaffar.sa139@gmail.com>
2023-04-08 15:40:08 -0400

    Create friendship and friend request tables.

--- tradester_backend/friendship/models.py
+++ tradester_backend/friendship/models.py
@@ -2,4 +2,11 @@ from django.db import models
 
 # Create your models here.
 class Friendship(models.Model):
-    pass
+    friendship_id = models.IntegerField(primary_key=True)
+    user_id = models.IntegerField()
+    other_user_id = models.IntegerField()
+
+class FriendRequest(models.Model):
+    request_id = models.IntegerField(primary_key=True)
+    send_user_id = models.IntegerField()
+    receiver_user_id = models.IntegerField()
