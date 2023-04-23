78b86b2c9534817943054a55f0f2089ac6691e5d
Owen King <tuj66158@temple.edu>
2023-04-23 12:53:59 -0400

    Testing holiday logic

--- Heroku/dbPopulation/importedFile.py
+++ Heroku/dbPopulation/importedFile.py
@@ -1,1 +1,9 @@
-print("Now this one printed")
+import datetime
+import pandas_market_calendars as mcal
+nyse = mcal.get_calendar('NYSE')
+today = datetime.date.today()
+
+valid_day = nyse.valid_days(start_date=today, end_date=today)
+
+if len(valid_day == 0 or (datetime.date.weekday() >= 5)):
+    print("It is a weekend or a stock holiday")
