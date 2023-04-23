cceaf1371f5487f72dd939663b92eacca69b3abc
voiced-dental-fricative <tum99326@temple.edu>
2023-04-23 01:14:00 -0400

    add testing for purchasing and selling stocks to a portfolio

--- tradester_backend/tradester/tests.py
+++ tradester_backend/tradester/tests.py
@@ -9,10 +9,11 @@ from rest_framework.authtoken.models import Token
 from rest_framework.test import APIClient
 from decimal import Decimal
 
-TEST_USER = 'test_user'
-TEST_PASS = 'test_pass'
+from django.conf import settings
 
 class TestUpdateOrderView(TestCase):
+    databases ={}
+    databases['render'] = settings.DATABASES['render']
     access_token = ""
     def setUp(self):
         self.client = APIClient()
@@ -76,46 +77,68 @@ class TestUpdateOrderView(TestCase):
         self.assertEqual(orders[1]['quantity'], 5)
         self.assertEqual(Decimal(orders[1]['price']), 200.00)
 
+class TestPortfolioFunctions(TestCase):
 
-class TestUserDelete(TestCase):
+    databases ={}
+    databases['render'] = settings.DATABASES['render']
+    access_token = ""
+    headers = None
+    def setUp(self):
+        self.client = APIClient()
+        existing_users = list(User.objects.values_list('username', flat=True))
+        if 'testuser' in existing_users:
+            # Delete existing user with the same username if it exists
+            User.objects.filter(username='testuser').delete()
 
-    def setup(self):
-        self.user = User.objects.create_user(username=TEST_USER, password=TEST_PASS)
-        Stock.objects.create(stock_symbol="MMM", current_price=110.30, company_name="3M")
+        self.user = User.objects.create_user(
+            username='testuser',
+            password='testpass'
+        ) 
 
-    def delete_user(self):
-        print('---delete user---')
-        client = APIClient()
-        loginResponse = client.post('/auth/token/',{'username':TEST_USER, 'password':TEST_PASS})
-        token = loginResponse.json()['access']
-        print('token: ', token)
-        headers = {'Authorization': 'Bearer ' + token}
-        deleteResponse = client.post('/tradester/delete_user_account/', headers=headers)
-        print(deleteResponse.status_code)
-        print(User.objects.get(username=TEST_USER))
+        self.client.login(username='testuser', password='testpass')
+        url = reverse('token_obtain_pair')
+        data = {'username': 'testuser', 'password': 'testpass'}
+        response = self.client.post(url, data, format='json')
+        self.access_token = response.data['access']
+        self.headers = {'Authorization': 'Bearer ' + self.access_token}
+        
 
-    def test_get_and_post_functionality(self):
-        #Endpoint takes stock_symbol, order_type, quantity, price
-        url = reverse('update_order_post', args=['TEST', 'BUY', 2, 20])
-        headers = {'Authorization': 'Bearer ' + self.access_token}
-        response = self.client.post(url, headers=headers)
+        #add a stock to the stock table
+        stock, created= Stock.objects.get_or_create(stock_symbol='TEST', current_price=20)
+        stock.save()
 
-        url = reverse('update_order_post', args=['TEST2', 'SELL', 5, 200])
-        headers = {'Authorization': 'Bearer ' + self.access_token}
-        response = self.client.post(url, headers=headers)
+    def test_purchase_stock(self):
 
-        url = reverse('update_order_get')
-        headers = {'Authorization': 'Bearer ' + self.access_token}
-        response = self.client.get(url, headers=headers)
-        orders = response.json()['orders']
+        #try to purchase an amount of TEST
+        stock = 'TEST'
+        price = 101.10
+        quantity = 2
+        response = self.client.get(f'/tradester/purchase_stock/?stock={stock}&price={price}&quantity={quantity}', headers=self.headers)
+        self.assertEquals(response.json()['total attempted'], price*quantity)
 
-        self.assertEqual(orders[0]['stock_symbol_id'], 'TEST')
-        self.assertEqual(orders[0]['order_type'], 'BUY') 
-        self.assertEqual(orders[0]['quantity'], 2)
-        self.assertEqual(Decimal(orders[0]['price']), 20.00)
-        self.assertEqual(orders[1]['stock_symbol_id'], 'TEST2')
-        self.assertEqual(orders[1]['order_type'], 'SELL') 
-        self.assertEqual(orders[1]['quantity'], 5)
-        self.assertEqual(Decimal(orders[1]['price']), 200.00)
+        #add a enough money to buy it
+        wallet = 400
+        response = self.client.get(f'/tradester/save_investment/?amount={wallet}', headers=self.headers)
+        self.assertEquals(response.json()['amount'], wallet)
 
+        #try again
+        response = self.client.get(f'/tradester/purchase_stock/?stock={stock}&price={price}&quantity={quantity}', headers=self.headers)
+        self.assertEquals(response.json()['purchase total'], price*quantity)
 
+    def test_sell_stock(self):
+
+        #add money to wallet
+        wallet = 400
+        response = self.client.get(f'/tradester/save_investment/?amount={wallet}', headers=self.headers)
+        self.assertEquals(response.json()['amount'], wallet)
+
+        #purchase a stock
+        stock = 'TEST'
+        price = 101.10
+        quantity = 2
+        response = self.client.get(f'/tradester/purchase_stock/?stock={stock}&price={price}&quantity={quantity}', headers=self.headers)
+        self.assertEquals(response.json()['purchase total'], price*quantity)
+
+        #sell that stock
+        response = self.client.get(f'/tradester/sell_stock/?stock={stock}&price={price}&quantity={quantity}', headers=self.headers)
+        self.assertEquals(response.status_code, status.HTTP_200_OK)
